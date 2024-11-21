afe5f912117b96ec3d125f8f0ec55875ce9dd8bf
ketikai <ketikai@idealstate.team>
2024-11-22 02:39:42 +0800

    chore(依赖库及相关版权和声明): 添加 asm 库，并补充与其相关的版权许可和声明……
    新增 Gradle 插件 shadowJar ，并配置相关构建任务；
    为工件添加 Java Agent 相关清单参数；
    为测试运行时添加 Sun 工具库。
    
    [skip ci]
    
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,10 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version("8.3.5")
 }
 
 group = "team.idealstate.sugar"
 version = "0.1.0-SNAPSHOT"
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
 
 java {
     toolchain {
@@ -26,10 +13,49 @@ java {
     }
 }
 
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.ow2.asm:asm:9.7.1")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly(files(File(javaToolchains.compilerFor {
+        languageVersion.set(java.toolchain.languageVersion)
+        vendor.set(java.toolchain.vendor)
+    }.get().executablePath.asFile.parentFile.parentFile, "lib/tools.jar")))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
+val manifestAttributes = mapOf(
+    "Premain-Class" to "team.idealstate.sugar.common.agent.AgentUtils",
+    "Agent-Class" to "team.idealstate.sugar.common.agent.AgentUtils",
+    "Can-Redefine-Classes" to true,
+    "Can-Retransform-Classes" to true,
+    "Can-Set-Native-Method-Prefix" to true,
+)
+
 tasks.jar {
-    dependsOn(tasks.test)
+    manifest {
+        attributes(manifestAttributes)
+    }
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    manifest {
+        attributes(manifestAttributes)
+    }
+    val pattern = "org.objectweb.asm"
+    relocate(pattern, "${project.group}.${pattern}")
 }
