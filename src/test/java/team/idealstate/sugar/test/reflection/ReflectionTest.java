ff9e46210e15ac9212aacdafbd5443d348f96083
ketikai <ketikai@idealstate.team>
2024-11-22 02:52:20 +0800

    refactor(reflection): 新增方法 annotation()，重命名 newInstance 等方法，从原 ReflectionHandler 提取接口并重新赋予实现与命名。
    新增方法 Reflection#annotation 可用于注解类的实例构造；
    新的接口 ReflectionInvocationHandler 提供默认的 hashCode 、equals 和 toString 实现。
    
    [skip ci]
    
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- src/test/java/team/idealstate/sugar/test/reflection/ReflectionTest.java
+++ src/test/java/team/idealstate/sugar/test/reflection/ReflectionTest.java
@@ -17,12 +17,12 @@
 package team.idealstate.sugar.test.reflection;
 
 import org.junit.jupiter.api.Test;
-import team.idealstate.sugar.reflection.Reflection;
 import team.idealstate.sugar.reflection.exception.ReflectionException;
 import team.idealstate.sugar.test.reflection.entity.Human;
 import team.idealstate.sugar.test.reflection.entity.ReflectHuman;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static team.idealstate.sugar.reflection.Reflection.reflect;
 
 public class ReflectionTest {
 
@@ -33,7 +33,7 @@ public class ReflectionTest {
 
     @Test
     public void newInstance() {
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, null);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, null);
         Human human = reflectHuman.newInstance(NAME, JUVENILE_AGE);
         assertEquals(new Human(NAME, JUVENILE_AGE), human);
     }
@@ -41,21 +41,21 @@ public class ReflectionTest {
     @Test
     public void name() {
         Human human = new Human(NAME, JUVENILE_AGE);
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, human);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, human);
         assertEquals(NAME, reflectHuman.name());
     }
 
     @Test
     public void age() {
         Human human = new Human(NAME, JUVENILE_AGE);
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, human);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, human);
         assertEquals(JUVENILE_AGE, reflectHuman.age());
     }
 
     @Test
     public void getAndSetAge() {
         Human human = new Human(NAME, JUVENILE_AGE);
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, human);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, human);
         assertEquals(JUVENILE_AGE, reflectHuman.getAndSetAge(ADULT_AGE));
         assertEquals(ADULT_AGE, human.age);
     }
@@ -63,53 +63,53 @@ public class ReflectionTest {
     @Test
     public void getName() {
         Human human = new Human(NAME, JUVENILE_AGE);
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, human);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, human);
         assertEquals(NAME, reflectHuman.getName());
     }
 
     @Test
     public void setName() {
         Human human = new Human(NAME, JUVENILE_AGE);
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, human);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, human);
         assertThrowsExactly(ReflectionException.class, () -> reflectHuman.setName(CHANGED_NAME));
     }
 
     @Test
     public void getAge() {
         Human human = new Human(NAME, JUVENILE_AGE);
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, human);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, human);
         assertEquals(JUVENILE_AGE, reflectHuman.getAge());
     }
 
     @Test
     public void setAge() {
         Human human = new Human(NAME, JUVENILE_AGE);
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, human);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, human);
         reflectHuman.setAge(ADULT_AGE);
         assertEquals(ADULT_AGE, human.age);
     }
 
     @Test
     public void LEGAL_AGE() {
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, null);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, null);
         assertThrowsExactly(ReflectionException.class, reflectHuman::LEGAL_AGE);
     }
 
     @Test
     public void getLegalAge() {
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, null);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, null);
         assertEquals(Human.getLegalAge(), reflectHuman.getLegalAge());
     }
 
     @Test
     public void defaultMethod() {
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, null);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, null);
         assertDoesNotThrow(reflectHuman::defaultMethod);
     }
 
     @Test
     public void unimplementedMethod() {
-        ReflectHuman reflectHuman = Reflection.newInstance(null, ReflectHuman.class, null);
+        ReflectHuman reflectHuman = reflect(null, ReflectHuman.class, null);
         assertThrowsExactly(ReflectionException.class, reflectHuman::unimplementedMethod);
     }
 }
