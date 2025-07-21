9757e4e3f4c9f987fd20c013056e2203caccee31
ketikai <ketikai@idealstate.team>
2025-07-21 19:25:54 +0800

    feat(annotation): 新增 @Provides 注解及处理器并增加 @NotNull 注解处理器
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- src/main/java/team/idealstate/sugar/validate/annotation/NotNull.java
+++ src/main/java/team/idealstate/sugar/validate/annotation/NotNull.java
@@ -21,8 +21,81 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import lombok.NonNull;
+import team.idealstate.sugar.logging.Log;
 
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface NotNull {}
+public @interface NotNull {
+
+    class AnnotationProcessor extends AbstractProcessor {
+
+        static {
+            hookLombok();
+        }
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        private static void hookLombok() {
+            try {
+                Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+                Field theUnsafe = unsafeClass.getDeclaredField("theUnsafe");
+                theUnsafe.setAccessible(true);
+                Object unsafe = theUnsafe.get(null);
+                Method staticFieldBase = unsafeClass.getDeclaredMethod("staticFieldBase", Field.class);
+                Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+                Method getAndSetObject =
+                        unsafeClass.getDeclaredMethod("getAndSetObject", Object.class, long.class, Object.class);
+                Class<?> handlerUtil =
+                        Class.forName("lombok.core.handlers.HandlerUtil", true, NonNull.class.getClassLoader());
+                Field field = handlerUtil.getDeclaredField("NONNULL_ANNOTATIONS");
+                field.setAccessible(true);
+                Object base = staticFieldBase.invoke(unsafe, field);
+                long offset = (long) staticFieldOffset.invoke(unsafe, field);
+                List list = new ArrayList((List) field.get(null));
+                list.add(NotNull.class.getName());
+                getAndSetObject.invoke(unsafe, base, offset, Collections.unmodifiableList(list));
+                field = handlerUtil.getDeclaredField("BASE_COPYABLE_ANNOTATIONS");
+                field.setAccessible(true);
+                base = staticFieldBase.invoke(unsafe, field);
+                offset = (long) staticFieldOffset.invoke(unsafe, field);
+                list = new ArrayList((List) field.get(null));
+                list.add(NotNull.class.getName());
+                list.add(Nullable.class.getName());
+                getAndSetObject.invoke(unsafe, base, offset, Collections.unmodifiableList(list));
+            } catch (ClassNotFoundException
+                    | IllegalAccessException
+                    | NoSuchFieldException
+                    | NoSuchMethodException
+                    | InvocationTargetException e) {
+                Log.error(e);
+            }
+        }
+
+        @Override
+        public Set<String> getSupportedAnnotationTypes() {
+            return Collections.singleton(NotNull.class.getName());
+        }
+
+        @Override
+        public SourceVersion getSupportedSourceVersion() {
+            return SourceVersion.latestSupported();
+        }
+
+        @Override
+        public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+            return false;
+        }
+    }
+}
